a35ee24d6fd6342e6573aca08defde18df19c6c1
Breno Oliveira De Sousa <154817681+NoirSteel@users.noreply.github.com>
2024-07-17 09:46:57 -0300

    Add files via upload

---
+++ PuriTea/index.js
@@ -0,0 +1,27 @@
+const lenis = new Lenis();
+
+lenis.on("scroll", ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
+
+function scrollToElement(elementSelector, instance = 0) {
+  const elements = document.querySelectorAll(elementSelector);
+  if (elements.length > instance) {
+    elements[instance].scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+// pode ser feito através do href com id
+
+link1.addEventListener("click", () => {
+  scrollToElement("#membros");
+});
+link2.addEventListener("click", () => {
+  scrollToElement("#footer");
+});
